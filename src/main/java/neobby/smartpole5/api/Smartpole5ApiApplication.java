bbe474ea0c03a04de46221a87d49dfef438e2d31
DESKTOP-HL37P2Q\lmh <vulpes1399@gmail.com>
2023-04-27 18:09:25 +0900

    Initial commit

---
+++ src/main/java/neobby/smartpole5/api/Smartpole5ApiApplication.java
@@ -0,0 +1,13 @@
+package neobby.smartpole5.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smartpole5ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Smartpole5ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neobby/smartpole5/api/Smartpole5ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package neobby.smartpole5.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smartpole5ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
